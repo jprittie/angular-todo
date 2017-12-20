5c2b043e87a7ec4688cd75ea40f46b7e3eba509a
jprittie <eyewallop@gmail.com>
2017-12-20 10:32:55 -0500

    Adds app-routing.module

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'todos',
+    pathMatch: 'full'
+  },
+  {
+    path: 'todos',
+    component: AppComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+  providers: []
+})
+export class AppRoutingModule {
+}
